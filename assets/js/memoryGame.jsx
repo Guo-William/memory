52f5803e7661294414a9ec83e6f1d75969879345
William Guo <williamguo@Williams-MacBook-Pro.local>
2018-01-28 23:03:29 -0500

    Trim down CSS
    Make for loop to make rows for each row of the game
    this way when resized they do not overflow to the next line

--- assets/js/memoryGame.jsx
+++ assets/js/memoryGame.jsx
@@ -88,20 +88,34 @@ class MemoryGame extends React.Component {
         let cardList = _.map(this.state.cards, (card, index) => {
             return <Card key={index} {...card} onclick={toggleReveal} />
         });
+        let displayDivs = []
+        for (let i = 0; i < 4; i++) {
+            const y = i * 4;
+            displayDivs.push(
+                <div key={i} className="row">
+                    {cardList[y + 0]}
+                    {cardList[y + 1]}
+                    {cardList[y + 2]}
+                    {cardList[y + 3]}
+                </div>
+            );
+        }
         return (
             <div>
                 <div className="row">
                     <div className="col-4" />
                     <div className="col-4">
-                        {cardList}
+                        {displayDivs}
                     </div>
                     <div className="col-4" />
                 </div>
                 <div className="row">
-                    <h2 className="text-center">clicks: {this.state.numClicks}</h2>
+                    <h2 className="col-12">clicks: {this.state.numClicks}</h2>
                 </div>
                 <div className="row">
-                    <Button className="text-center" onClick={() => this.setState(makeNewGameState())}>Reset</Button>
+                    <div className="col-12 text-center">
+                        <Button onClick={() => this.setState(makeNewGameState())}>Reset</Button>
+                    </div>
                 </div>
             </div>
         );
@@ -110,7 +124,7 @@ class MemoryGame extends React.Component {
 
 function Card(props) {
     const { reveal, matched, letter, onclick, id } = props;
-    const defaultClassNames = "col-3 card ";
+    const defaultClassNames = "col-3 tile ";
     const extraClassName = matched ? "bg-success" : "";
     const newClassNames = defaultClassNames.concat(extraClassName);
 
